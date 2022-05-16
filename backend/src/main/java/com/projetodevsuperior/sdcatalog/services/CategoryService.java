686345c832eb04b812a1470ff3c7eee48812193f
Jean Fernandine <jfernandine@gmail.com>
2022-05-16 14:35:29 -0300

    DATABASE Seeding

--- backend/src/main/java/com/projetodevsuperior/sdcatalog/services/CategoryService.java
+++ backend/src/main/java/com/projetodevsuperior/sdcatalog/services/CategoryService.java
@@ -2,8 +2,10 @@ package com.projetodevsuperior.sdcatalog.services;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.projetodevsuperior.sdcatalog.entities.Category;
 import com.projetodevsuperior.sdcatalog.repositories.CategoryRepository;
@@ -14,6 +16,7 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
+	@Transactional(readOnly=true)
 	public List<Category> findAll() {
 		return repository.findAll();
 	}
