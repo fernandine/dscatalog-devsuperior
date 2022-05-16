a9347ec77625317f78f2797e148fb98a0a4a2c02
Jean Fernandine <jfernandine@gmail.com>
2022-05-16 11:08:21 -0300

    Backend project created

---
+++ backend/src/main/java/com/projetodevsuperior/sdcatalog/SdcatalogApplication.java
@@ -0,0 +1,13 @@
+package com.projetodevsuperior.sdcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdcatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdcatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/projetodevsuperior/sdcatalog/SdcatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetodevsuperior.sdcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdcatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
