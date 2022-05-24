58fc192c8a0de040fbe2cd37d76c56869a2282bc
Jean Fernandine <jfernandine@gmail.com>
2022-05-24 18:06:47 -0300

    testando findAllPaged do ProductService

--- backend/src/test/java/com/projetodevsuperior/sdcatalog/services/ProductServiceTests.java
+++ backend/src/test/java/com/projetodevsuperior/sdcatalog/services/ProductServiceTests.java
@@ -13,10 +13,13 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.projetodevsuperior.sdcatalog.DTOs.ProductDTO;
 import com.projetodevsuperior.sdcatalog.entities.Product;
 import com.projetodevsuperior.sdcatalog.repositories.ProductRepository;
 import com.projetodevsuperior.sdcatalog.services.exceptions.DatabaseException;
@@ -60,6 +63,21 @@ public class ProductServiceTests {
 		Mockito.doThrow(DataIntegrityViolationException.class).when(repository).deleteById(dependentId);
 		
 	}
+	
+	
+	@Test
+	public void findAllPagedShouldReturnPage() {
+		
+		Pageable pageable = PageRequest.of(0, 10);
+		Page<ProductDTO> result = service.findAllPaged(pageable);
+		
+		Assertions.assertNotNull(result);
+		Mockito.verify(repository, Mockito.times(1)).findAll(pageable);
+		
+	}
+	
+	
+	
 	
 	@Test
 	public void deleteShouldThrowDatabaseExceptionWhenDependentId() {
