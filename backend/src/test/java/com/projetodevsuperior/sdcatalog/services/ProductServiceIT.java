5ff3b5a84bb0256bb0d432df7b7aec51f1e8f099
Jean Fernandine <jfernandine@gmail.com>
2022-05-26 08:39:25 -0300

    Primeiros testes de integracao

---
+++ backend/src/test/java/com/projetodevsuperior/sdcatalog/services/ProductServiceIT.java
@@ -0,0 +1,52 @@
+package com.projetodevsuperior.sdcatalog.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.projetodevsuperior.sdcatalog.repositories.ProductRepository;
+import com.projetodevsuperior.sdcatalog.services.exceptions.ResourceNotFoundException;
+
+@SpringBootTest
+@Transactional
+public class ProductServiceIT {
+
+	@Autowired
+	private ProductService service;
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	private Long existingId;
+	private Long nonExistingId;
+	private Long countTotalProducts;
+	
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		existingId = 1L;
+		nonExistingId = 1000L;
+		countTotalProducts = 25L;
+	}
+	
+	@Test
+	public void deleteShouldDeleteResourceWhenIdExists() {
+		
+		service.delete(existingId);
+		
+		Assertions.assertEquals(countTotalProducts - 1, repository.count());
+	}
+	
+	@Test
+	public void deleteShouldResourceNotFoundExceptionWhenIdDoesNotExists() {
+		
+		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+		service.delete(nonExistingId);
+		
+	});
+	
+	}
+}
